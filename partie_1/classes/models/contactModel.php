1d3c869e5ed6a0f17424d3935bdecc107261292b
TRO KOPE EMMANUEL JUNIOR <44979165+troemmanuel@users.noreply.github.com>
2024-01-02 23:08:48 +0100

    Update contactModel.php

--- partie_1/classes/models/contactModel.php
+++ partie_1/classes/models/contactModel.php
@@ -1,13 +1,13 @@
 <?php
     class Contact {
-        private $id_contact;
+        private $id;
         private $nom;
         private $prenom;
         private $email;
         private $numero_tel;
 
-        public function __construct($id_contact, $nom, $prenom, $email, $numero_tel) {
-            $this->id_contact = $id_contact;
+        public function __construct($id, $nom, $prenom, $email, $numero_tel) {
+            $this->id = $id;
             $this->nom = $nom;
             $this->prenom = $prenom;
             $this->email = $email;
@@ -15,14 +15,14 @@
         }
 
         // Getters
-        public function getIdContact() { return $this->id_contact; }
+        public function getIdContact() { return $this->id; }
         public function getNom() { return $this->nom; }
         public function getPrenom() { return $this->prenom; }
         public function getEmail() { return $this->email; }
         public function getNumeroTel() { return $this->numero_tel; }
 
         // Setters
-        public function setIdContact($id_contact) { $this->id_contact = $id_contact; }
+        public function setId($id) { $this->id = $id; }
         public function setNom($nom) { $this->nom = $nom; }
         public function setPrenom($prenom) { $this->prenom = $prenom; }
         public function setEmail($email) { $this->email = $email; }
