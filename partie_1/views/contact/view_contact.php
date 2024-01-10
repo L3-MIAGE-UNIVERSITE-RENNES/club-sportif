ae14189acef4e60f5e75def859581749944497e3
OliveAudrey <olive-audrey.irakoze@etudiant.univ-rennes.fr>
2024-01-10 18:49:30 +0100

    Update contact views

--- partie_1/views/contact/view_contact.php
+++ partie_1/views/contact/view_contact.php
@@ -1,22 +1,43 @@
 <?php require('../../controllers/auth/guard.php'); ?><!DOCTYPE html>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Détails contact</title>
-    <link rel="stylesheet" href="../css/styles.css">
-</head>
-<body>
-<h1>Détails contact</h1>
-<a href="ListContactController.php">Retour à la liste des contacts</a>
+<?php ob_start(); ?>
+<?php if (isset($contact)): ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <div class="card mt-4">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Nom :</strong></h5>
+                    <p class="card-text"><?php echo $contact->getNom(); ?></p>
+                </div>
+            </div>
 
-<?php if ($contact): ?>
-    <p><strong>Nom :</strong> <?php echo $contact->getNom(); ?></p>
-    <p><strong>Prenom :</strong> <?php echo $contact->getPrenom(); ?></p>
-    <p><strong>Email :</strong> <?php echo $contact->getEmail(); ?></p>
-    <p><strong>Numero telephone :</strong> <?php echo $contact->getNumeroTel() ?></p>
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Prenom :</strong></h5>
+                    <p class="card-text"><?php echo $contact->getPrenom(); ?></p>
+                </div>
+            </div>
+
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Email :</strong></h5>
+                    <p class="card-text"><?php echo $contact->getEmail(); ?></p>
+                </div>
+            </div>
+
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Numero telephone :</strong></h5>
+                    <p class="card-text"><?php echo $contact->getNumeroTel(); ?></p>
+                </div>
+            </div>
+            <a href="ListContactController.php" class="btn btn-primary mt-3">Retour à la liste des contacts</a>
+        </div>
+    </div>
+</div>
 <?php else: ?>
-    <p>Le contact n'a pas été trouvé.</p>
+    <p class="mt-3">Le contact n'a pas été trouvé.</p>
 <?php endif; ?>
-</body>
-</html>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('../../views/layout.php') ?>
